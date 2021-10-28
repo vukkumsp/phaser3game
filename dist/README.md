ae955da3f784ba56d2d729d00e9928aa065a6fb5
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-28 19:41:38 +0000

    StarFall game is completed;

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+<!-- https://www.freecodecamp.org/news/how-to-build-a-simple-game-in-the-browser-with-phaser-3-and-typescript-bdc94719135/ -->
---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import "phaser";
+import { GameScene } from "./gameScene";
+import { ScoreScene } from "./scoreScene";
+import { WelcomeScene } from "./welcomeScene";
+
+// https://phaser.discourse.group/t/how-use-phaser-3-17s-new-config-object-in-typescript/2548
+// GameConfig not found then use Phaser.Types.Core.GameConfig inplace of GameConfig
+
+const config: Phaser.Types.Core.GameConfig = {
+  // title: "Starfall",
+  // width: 800,
+  // height: 600,
+  // parent: "game",
+  // backgroundColor: "#18216D"
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [WelcomeScene, GameScene, ScoreScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#000033"
+};
+export class StarfallGame extends Phaser.Game {
+    // Phaser.Types.Core.GameConfig
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+window.onload = () => {
+  var game = new StarfallGame(config);
+};
